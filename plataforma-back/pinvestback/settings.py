68abe436bf38842705a5a057d0212b363e4a3c17
pop <pop@pop-os.localdomain>
2023-04-13 21:59:55 -0300

    feat/37/migrando-para-postgresql

--- plataforma-back/pinvestback/settings.py
+++ plataforma-back/pinvestback/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'pinvestback.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'plataforma',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
