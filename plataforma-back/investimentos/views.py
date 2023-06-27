468055da1f825cefec422954262e3b33826d329b
galleguillos <luisfelipegalleguillos@hotmail.com>
2023-06-27 15:22:59 -0300

    ajuste sincronização brapi

--- plataforma-back/investimentos/views.py
+++ plataforma-back/investimentos/views.py
@@ -18,16 +18,26 @@ class InvestimentosView(APIView):
     def get(self, _):
         investimentos = Investimentos.objects.all().order_by('name')
         serializer = InvestimentosSerializer(investimentos, many=True)
+        # self.sync()
         return Response(serializer.data)
     
     def sync(self):
         data = requests.get("https://brapi.dev/api/quote/list?sortBy=close&sortOrder=desc")
         data = data.json()
         for stock in data['stocks']:
-            Investimentos.objects.update_or_create(
-                code=stock['stock'],
-                name=stock['name'],
-                logo=stock['logo'],
-                value=stock['close']
-            )
+            try:
+                obj = Investimentos.objects.get(code=stock['stock'])
+                for key, value in stock.items():
+                    setattr(obj, key, value)
+                obj.save()
+            except Investimentos.DoesNotExist:
+                new_values = {
+                    'code':stock['stock'],
+                    'name':stock['name'],
+                    'logo':stock['logo'],
+                    'value':stock['close']
+                }
+                obj = Investimentos(**new_values)
+                obj.save()
+
         return data
