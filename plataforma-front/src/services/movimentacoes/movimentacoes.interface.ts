4a3c54dc681eee99a4aea2b9de9e74b050fe9222
galleguillos <luisfelipegalleguillos@hotmail.com>
2023-06-28 18:59:27 -0300

    grafico gastos x tempo

--- plataforma-front/src/services/movimentacoes/movimentacoes.interface.ts
+++ plataforma-front/src/services/movimentacoes/movimentacoes.interface.ts
@@ -20,6 +20,21 @@ export interface SaldoCategoria {
     categoria:string
     saldo:number
 }
+
+export interface SaldoTempo {
+    key:string
+    saldo:number
+}
+
+export enum SaldoTempoType {
+    year='year',
+    month='month',
+    day='day'
+}
+export interface DistribuicaoSaldoTempo {
+    tipo:SaldoTempoType,
+    values:SaldoTempo[]
+}
 export interface Categoria {
     key:string
     label:string
